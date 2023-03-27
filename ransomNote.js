27112308078cdfb45ac1bc8e28827cdb3878f10c
Grebllah <Grebllah@MacBook-Pro.ht.home>
2023-03-27 17:14:06 -0400

    ransomNote.js completed

--- ransomNote.js
+++ ransomNote.js
@@ -20,3 +20,27 @@
 //     1 <= ransomNote.length, magazine.length <= 105
 //     ransomNote and magazine consist of lowercase English letters.
 
+const canConstruct = (ransomNote, magazine) => {
+    let sortedNote = ransomNote.split("").sort()
+    let sortedMag = magazine.split("").sort()
+    const magObject = {}
+    for (i = 0; i < sortedMag.length; i++) {
+        let key = sortedMag[i]
+        if (!magObject[key]) magObject[key] = 1
+        else magObject[key]++
+    }
+    const magLetters = Object.entries(magObject)
+    let currentLetter = ""
+    for (let i = 0; i < sortedNote.length; i++) {
+        currentLetter = sortedNote[i]
+            if (magObject[currentLetter]) {
+                magObject[currentLetter]--
+            } else {
+                console.log("false")
+                return false
+            }
+        }
+    console.log("true")
+    return true
+}
+canConstruct("dsfdsfdppsfsdf", "dsafdsfasdfadsfasdfsdfsdf")
